129162e8109a6982f80a686c6691233742a93e20
savnkk_manjaro <39103342+natask@users.noreply.github.com>
2020-09-27 03:41:49 -0700

    (feat): added comments, revamped variables.
    fixed bug with `org-roam--open-file-rename-tag-and-close-file` well.

--- org-roam-tag-management.el
+++ org-roam-tag-management.el
@@ -1,6 +1,38 @@
-;; org-roam-tag-management.el
+;;; org-roam-tag-management.el --- Manage Tags of Org Roam -*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright © 2020 Natnael Kahssay <thisnkk@gmail.com>
+
+;; Author: Natnael Kahssay <thisnkk@gmail.com>
+;; URL: https://github.com/natask/org-roam
+;; Keywords: org-mode, roam, convenience
+;; Version: 1.2.1
+;; Package-Requires: ((emacs "26.1") (dash "2.13") (f "0.17.2") (s "1.12.0") (org "9.3") (emacsql "3.0.0") (emacsql-sqlite3 "1.0.2"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; This library provides the org-roam-tag-management functionality for org-roam
+;;; Code:
+;;;; Library Requires
 (require 'org-roam-db)
 
+;;; variables
 (defcustom org-roam-tag-data-types '(
                                   (fuzzy . ((format . "%%%s%%")
                                             (replace . (lambda (keyword before after)
@@ -15,20 +47,22 @@
                                                         ))
                                             ))
                                   )
-  "search type. fuzzy or exact or any other function.")
+  "Types on how to treat tag search. fuzzy or exact or any other custom type."
+  :type '(alist (group (alist (group sexp)))) 
+  :require 'org-roam-mode
+  :group 'org-roam
+  )
 
 (defcustom org-roam-tag-data-type 'fuzzy
-   "search type"         )
+  "Default tag search type."
+  :type 'symbol
+  :require 'org-roam-mode
+  :group 'org-roam
+)
 
-(defun org-roam-get-all-tags()
-  (delete-dups (-flatten (org-roam-db-query
-                          [:select [tags]
-                           :from tags] ))))
-
-(defun org-roam-number-of-files-with-tag (tag)
-  (length (org-roam-files-with-tag tag)))
-
+;;; Variable access functions
 (defun org-roam--get-format-string ()
+  "Get the format string of current tag data type."
    (condition-case err
        (cdr (assoc 'format (cdr (assoc org-roam-tag-data-type org-roam-tag-data-types))))
    (error (message "make sure format is defined in %s in org-roam-tag-data-types.\n%s" org-roam-tag-data-type err) (signal (car err) (cdr err))
@@ -36,25 +70,42 @@
   )
 
 (defun org-roam--get-replace-function ()
+  "Get the replace function of current tag data type."
   (condition-case err
       (cdr (assoc 'replace (cdr (assoc org-roam-tag-data-type org-roam-tag-data-types))))
     (error (message "make sure replace is defined in %s in org-roam-tag-data-types.\n%s" org-roam-tag-data-type err) (signal (car err) (cdr err))
            ))
   )
 
-(defun org-roam-files-with-tag (tag)
-  (org-roam-files-with-type-tag tag (org-roam--get-format-string))
-  )
+;;; Tag functions
+;;;; Tag all functions
+(defun org-roam-get-all-tags()
+  "Get all org roam tags."
+  (delete-dups (-flatten (org-roam-db-query
+                          [:select [tags]
+                           :from tags] ))))
 
-(defun org-roam-files-with-type-tag (tag f)
+;;;; Tag file functions
+(defun org-roam-files-with-type-tag (tag format-string)
+  "List files that match `tag' under search using `format-string'."
   (org-roam-db-query
    [:select [file]
     :from tags
-    :where (like tags  $r1)] (format f tag)))
+    :where (like tags  $r1)] (format format-string tag)))
 
 
+(defun org-roam-files-with-tag (tag)
+  "List files that match `tag' under search using current `org-roam-tag-data-type'."
+  (org-roam-files-with-type-tag tag (org-roam--get-format-string))
+  )
+
+(defun org-roam-number-of-files-with-tag (tag)
+  "Find number of tiles with `tag'."
+  (length (org-roam-files-with-tag tag)))
+
+;;;; Tag rename functions
 (defun org-roam--buffer-rename-tag (keywords before after)
-  "takes a array of string and replaces a value that matches."
+  "Takes a array of strings, `keywords' and replaces string matching`before' with `after'."
   (mapcar #'(lambda (x)
              ( let (
                     ( val (funcall (org-roam--get-replace-function) x before after))
@@ -66,6 +117,8 @@
           keywords))
 
 (defun org-roam--buffer-find-and-rename-tag (before after)
+  "Rename tag from `before' to `after' on current buffer.
+   Currently only supports tags defined under `roam-tags' property."
   (let (keywords after-list concat-string)
     (setq keywords (split-string-and-unquote (car (cdr (car (org-collect-keywords '("roam_tags")))))))
     (setq after-list (org-roam--buffer-rename-tag keywords before after))
@@ -74,9 +127,10 @@
     ))
 
 (defun org-roam--open-file-rename-tag-and-close-file (file before after)
-  (org-roam-with--file file
+  "Rename tag from `before' to `after' on file."
+  (org-roam--with-file file
   (let (to-write)
-    (find-file file)
+    (find-file-noselect file)
     (setq to-write (org-roam--buffer-find-and-rename-tag before after))
     (goto-char (point-min))
     (re-search-forward "^#\\+roam_tags:.*" nil t)
@@ -85,6 +139,8 @@
     )))
 
 (defun org-roam-rename-tag (before after)
+  "Rename tag from `before' to `after' on all org roam files.
+   Currently only supports tags defined under `roam-tags' property."
   (let ((files-to-rename-tag (-flatten (org-roam-files-with-tag before))))
     (save-window-excursion
     (save-excursion
